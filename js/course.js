7e4dbfc9f572e0cb3b84b108e5548fce0f5671c6
mando2002317 <164183738+mando2002317@users.noreply.github.com>
2025-07-02 16:19:12 +0300

    Add files via upload

---
+++ js/course.js
@@ -0,0 +1,217 @@
+/* 
+
+GPA Calculator by Abdelrahman Metwally
+MED SPACE
+
+*/
+
+var courseName = "Faculty Of Medicine - University of MTI";
+
+var courseUnits = [
+  {
+    year: "1",
+    sems: [
+      {
+        sem: "1",
+        subs: [
+          {
+            name: "General Anatomy",
+            id: "ANA3",
+          },
+          {
+            name: "Physiology",
+            id: "PHY2",
+          },
+          {
+            name: "Histology",
+            id: "HIS2",
+          },
+          {
+            name: "Genatics Module",
+            id: "GEN2",
+          },
+          {
+            name: "Biochemistry",
+            id: "BIO4",
+          },
+        ],
+      },
+      {
+        sem: "2",
+        subs: [
+          {
+            name: "Pathology",
+            id: "PATH3",
+          },
+          {
+            name: "Pharmacology",
+            id: "PH2",
+          },
+          {
+            name: "Blood Module",
+            id: "BO6",
+          },
+          {
+            name: "Muscloskeletal System I",
+            id: "MUI5",
+          },
+          {
+            name: "Neurosence I Module",
+            id: "NE2",
+          },
+          {
+            name: "Professionalism",
+            id: "PRO2",
+          },
+          {
+            name: "ECE I",
+            id: "ECE1",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    year: "2",
+    sems: [
+      {
+        sem: "3",
+        subs: [
+          {
+            name: "Muscloskeletal System II",
+            id: "MU4",
+          },
+          {
+            name: "Respiratory System",
+            id: "RES5",
+          },
+          {
+            name: "Cardiovascular System",
+            id: "CVS7",
+          },
+          {
+            name: "ECE II",
+            id: "ECE II1",
+          },
+          {
+            name: "Psychology I",
+            id: "PSY1",
+          },
+        ],
+      },
+      {
+        sem: "4",
+        subs: [
+          {
+            name: "GIT Module",
+            id: "GIT7",
+          },
+          {
+            name: "URO Genital Module",
+            id: "URO4",
+          },
+          {
+            name: "Reproductive Module",
+            id: "REP5",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    year: "3",
+    sems: [
+      {
+        sem: "5",
+        subs: [
+          {
+            id: "MUSIII 5",
+            name: "Muscloskeletal System III",
+          },
+          {
+            id: "CNS8",
+            name: "CNS Module",
+          },
+          {
+            id: "END4",
+            name: "Endocrine Module",
+          },
+          {
+            id: "ECE III1",
+            name: "ECE III",
+          },
+          {
+            id: "PSYII1",
+            name: "Psychology II",
+          },
+
+ 
+        ],
+      },
+      {
+        sem: "6",
+        subs: [
+          {
+            id: "SENG 31262",
+            name: "OPH Round",
+          },
+          {
+            id: "SENG 34222",
+            name: "ENT ROUND",
+          },
+          {
+            id: "SENG 34213",
+            name: "Communitny",
+          },
+          {
+            id: "SENG 32216",
+            name: "Investigation",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    year: "4",
+    sems: [
+      {
+        sem: "7",
+        subs: [
+          {
+            name: "PED I ",
+            id: "SENG 41212",
+          },
+          {
+            name: "OBS ",
+            id: "SENG 41222",
+          },
+          {
+            name: "Medicine",
+            id: "SENG 41233",
+          },
+          {
+            name: "Forensic & Toxocology",
+            id: "SENG 41242",
+          },
+        ],
+      },
+      {
+        sem: "8",
+        subs: [
+          {
+            name: "PED II",
+            id: "SENG 42212",
+          },
+          {
+            name: "GYNA",
+            id: "SENG 43216",
+          },
+          {
+            name: "Surgery I",
+            id: "SENG 42222",
+          },
+        ],
+      },
+    ],
+  },
+];
---
+++ js/grades.js
@@ -0,0 +1,35 @@
+var gradeValues = [
+    {
+        name: "A+",
+        val: 4.0
+    },
+    {
+        name: "A",
+        val: 3.67
+    },
+    {
+        name: "B+",
+        val: 3.33
+    },
+    {
+        name: "B",
+        val: 3.00
+    },
+    {
+        name: "C+",
+        val: 2.67
+    },
+    {
+        name: "C",
+        val: 2.33
+    },
+
+    {
+        name: "D",
+        val: 2.0
+    },
+    {
+        name: "F",
+        val: 0.0
+    }
+];
